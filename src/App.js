af5aecfb31f0c3aba3590fd4b64442a9c0085044
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2023-03-07 00:00:31 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './style.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
+import { Home, Blog, Shop, Contact } from './pages';
 export default function App() {
   return (
     <>
-      <Header />
-      <Navbar />
+      <BrowserRouter>
+        <Header />
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/blog" element={<Blog />} />
+          <Route exact path="/shop" element={<Shop />} />
+          <Route exact path="/contact" element={<Contact />} />
+          {/*<Route exact path="/products" element={<Products />} />
+          <Route exact path="/products/:id" element={<SingleProduct />} />
+          <Route exact path="/checkout" element={<Checkout />} />
+
+           <PrivateRoute
+            path="/private"
+            component={Private}
+            element={<Checkout />}
+          /> */}
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
