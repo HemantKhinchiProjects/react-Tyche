6f025719d8502324af2e9d979c6dd5e09a490ffe
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2023-03-10 00:11:30 +0530

    1 file modified, 1 added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import Menu from './utils/Menu';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="main-navigation">
       <ul className="sf-menu">
+        {Menu.map((link) => {
+          const { id, text, url } = link;
+          return <li></li>;
+        })}
         <li>
           <a href="#">Menu Item 1</a>
         </li>
---
+++ src/utils/Menu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import { GiCompass, GiDiamondHard, GiStabbedNote } from 'react-icons/gi';
+export const links = [
+  {
+    id: 1,
+    text: 'home',
+    url: '/',
+  },
+  {
+    id: 2,
+    text: 'Shop',
+    url: '/Shop',
+  },
+  {
+    id: 3,
+    text: 'Contact',
+    url: '/Contact',
+  },
+  {
+    id: 4,
+    text: 'Blog',
+    url: '/Blog',
+  },
+];
