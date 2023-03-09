f0893e1ac41f8f948b18250627e96ab118605d22
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2023-03-10 00:25:51 +0530

    1 file modified

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import './Navbar.css';
 import { navbarMenus } from '../../utils/constants';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="main-navigation">
-      <ul className="sf-menu">
-        {navbarMenus.map((link) => {
-          const { id, text, url } = link;
-          return (
-            <li key={id}>
-              <Link to={url}>{text}</Link>
-            </li>
-          );
-        })}
-      </ul>
+      <div className="container">
+        <ul className="sf-menu">
+          {navbarMenus.map((link) => {
+            const { id, text, url } = link;
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </nav>
   );
 };
